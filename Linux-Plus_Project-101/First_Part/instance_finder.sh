7258fc42439c428b19d55548dd0c07209141f1fe
TerryA <teeaye2212@gmail.com>
2022-09-13 18:21:54 -0500

    intial commit

---
+++ Linux-Plus_Project-101/First_Part/instance_finder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+# This script is used to find instance ids terminated by a particular user
+# Append file name after this script in terminal
+
+read -p "Enter User's Name: " user 
+
